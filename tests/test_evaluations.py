1cba55672c2ded1a3e1331ee7da6560709e19f5c
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-11-06 12:52:03 +0100

    Adds three variations of the same extraction to improve the test a bit

--- tests/test_evaluations.py
+++ tests/test_evaluations.py
@@ -7,6 +7,11 @@ def test_evaluations():
     
     with open(os.path.dirname(os.path.realpath(__file__)) + os.sep + "truth_test_file_10.1016--j.orgel.2017.01.022.json") as f:
         truth = json.load(f)
-    with open(os.path.join(THIS_DIR,  "extraction_test_file_10.1016--j.orgel.2017.01.022.json")) as f:
-        extraction = json.load(f)
-    assert Evaluations(truth, extraction).score == 0.9998021369212505
+    with open(os.path.join(THIS_DIR,  "extraction_best_test_file_10.1016--j.orgel.2017.01.022.json")) as f:
+        best_extraction = json.load(f)
+    with open(os.path.join(THIS_DIR,  "extraction_mid_test_file_10.1016--j.orgel.2017.01.022.json")) as f:
+        mid_extraction = json.load(f)
+    with open(os.path.join(THIS_DIR,  "extraction_worst_test_file_10.1016--j.orgel.2017.01.022.json")) as f:
+        worst_extraction = json.load(f)
+    assert Evaluations(truth, best_extraction).score > Evaluations(truth, mid_extraction).score
+    assert Evaluations(truth, mid_extraction).score > Evaluations(truth, worst_extraction).score
