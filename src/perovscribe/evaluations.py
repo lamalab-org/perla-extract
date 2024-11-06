51bfbdb424e643ea96966764bc9028021973d4d2
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-11-06 12:36:40 +0100

    Adds type hints

--- src/perovscribe/evaluations.py
+++ src/perovscribe/evaluations.py
@@ -4,7 +4,7 @@ class Evaluations:
     deep_results = None
     score = None
 
-    def __init__(self, truth, extraction):
+    def __init__(self, truth: dict, extraction: dict):
         """Calculates a score between 0 and 1 using deepdiff. 1 being the best."""
         self.deep_results = DeepDiff(truth, extraction, ignore_order=True, get_deep_distance=True, significant_digits=1)
         self.score = 1-self.deep_results["deep_distance"] if "deep_distance" in self.deep_results else 1
