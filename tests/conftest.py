23ad96696a9450467adaeb6ae1d299aab08763fc
Kevin Maik Jablonka <mail@kjablonka.com>
2024-11-06 12:29:55 +0100

    add test for pymupdf

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+import os
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@pytest.fixture()
+def get_nat_comm_7139_file():
+    return os.path.join(THIS_DIR, 'test_files', 'nat_comm_7139.pdf')
---
+++ tests/preprocessing/test_pymupdf.py
@@ -0,0 +1,7 @@
+from perovscribe.preprocessing.pymupdf import PyMuPDFPreprocessor
+
+def test_pymupdf(get_nat_comm_7139_file):
+    preprocessor = PyMuPDFPreprocessor('pymupdf', cache_dir_root=None, use_cache=False)
+    text = preprocessor.pdf_to_text(get_nat_comm_7139_file)
+    assert len(text) > 0
+    assert isinstance(text, str)
