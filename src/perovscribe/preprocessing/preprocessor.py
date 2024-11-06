84595fe39ad4c472f5c5a621cda658fb3dff079b
Kevin Maik Jablonka <mail@kjablonka.com>
2024-11-06 12:46:29 +0100

    make imports work

--- src/perovscribe/preprocessing/preprocessor.py
+++ src/perovscribe/preprocessing/preprocessor.py
@@ -6,7 +6,7 @@ def get_preprocessor(name, cache_dir_root: PathType, use_cache: bool):
     if name == "nougat":
         raise NotImplementedError("Nougat preprocessing is not implemented")
     elif name == "pymupdf":
-        from perovscribe.preprocessing.pymupdf import PyMuPDFPreprocessor
+        from perovscribe.preprocessing.pymupdf_processor import PyMuPDFPreprocessor
 
         return PyMuPDFPreprocessor(
             name, cache_dir_root=cache_dir_root, use_cache=use_cache
--- tests/preprocessing/test_pymupdf.py
+++ tests/preprocessing/test_pymupdf.py
@@ -1,4 +1,4 @@
-from perovscribe.preprocessing.pymupdf import PyMuPDFPreprocessor
+from perovscribe.preprocessing.pymupdf_processor import PyMuPDFPreprocessor
 
 
 def test_pymupdf(get_nat_comm_7139_file):
