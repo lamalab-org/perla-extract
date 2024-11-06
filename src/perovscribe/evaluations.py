a04b2b4f2313f774b33499ad2c308fedb3adafc7
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-11-06 10:58:34 +0100

    Adds a 1-significant param in deepdiff to only check 1-sig in nums

--- src/perovscribe/evaluations.py
+++ src/perovscribe/evaluations.py
@@ -6,6 +6,6 @@ class Evaluations:
 
     def __init__(self, truth, extraction):
         """Calculates a score between 0 and 1 using deepdiff. 1 being the best."""
-        self.deep_results = DeepDiff(truth, extraction, ignore_order=True, get_deep_distance=True)
+        self.deep_results = DeepDiff(truth, extraction, ignore_order=True, get_deep_distance=True, significant_digits=1)
         self.score = 1-self.deep_results["deep_distance"] if "deep_distance" in self.deep_results else 1
 
