154b8b9721b8d212586bbb304f3239ba86da9f3e
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-11-06 12:33:59 +0100

    Update tests/test_evaluations.py
    Co-authored-by: Kevin M Jablonka <32935233+kjappelbaum@users.noreply.github.com>

--- tests/test_evaluations.py
+++ tests/test_evaluations.py
@@ -7,6 +7,6 @@ def test_evaluations():
     
     with open(os.path.dirname(os.path.realpath(__file__)) + os.sep + "truth_test_file_10.1016--j.orgel.2017.01.022.json") as f:
         truth = json.load(f)
-    with open(os.path.dirname(os.path.realpath(__file__)) + os.sep + "extraction_test_file_10.1016--j.orgel.2017.01.022.json") as f:
+    with open(os.path.join(THIS_DIR,  "extraction_test_file_10.1016--j.orgel.2017.01.022.json")) as f:
         extraction = json.load(f)
     assert Evaluations(truth, extraction).score == 0.9998021369212505
