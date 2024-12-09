3a1efff3642e4cea1437483775d5d12ad2f24a92
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-12-09 20:06:57 +0100

    Update tests/postprocessing/test_postprocessing.py
    Co-authored-by: Kevin M Jablonka <32935233+kjappelbaum@users.noreply.github.com>

--- tests/postprocessing/test_postprocessing.py
+++ tests/postprocessing/test_postprocessing.py
@@ -2,5 +2,4 @@ from perovscribe.postprocessing import normalize
 
 
 def test_normalize(normalize_input, normalize_output):
-    print(normalize(normalize_input), "\n=====\n", normalize_output)
     assert normalize(normalize_input) == normalize_output
